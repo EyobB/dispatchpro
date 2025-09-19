6457c96cfb9de3a2957bed88ea0fb09ef75e3339
Eyob <eyob.beshah@gmail.com>
2025-09-18 21:40:16 -0500

    deploying

---
+++ docs/chunk-4DXCQ267.js
@@ -0,0 +1,1 @@
+import{Va as n,kb as o,lb as s,zb as a}from"./chunk-OP2CD5JB.js";var p=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-payments"]],decls:2,vars:0,template:function(e,i){e&1&&(o(0,"p"),a(1,"payments works!"),s())},encapsulation:2})};export{p as PaymentsComponent};
